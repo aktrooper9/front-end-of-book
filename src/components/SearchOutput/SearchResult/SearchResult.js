5f8e3255902f491058b7bc5a216a20da51d76c34
League Student <league@iMac-16.attlocal.net>
2022-05-29 16:23:22 -0700

    CSS for the output (b/c the auto css does not work like intended)

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -4,12 +4,10 @@ import classes from './SearchResult.module.css';
 const SearchResult = (props) => {
     console.log(props);
     return (
-        <article className={classes.MRData}>
-            <hr />
+        <article className={classes.MRData} style={{borderRadius: 50 + "px", margin: "auto", background: "#A9A9A9", paddingTop: 1 + "em", paddingBottom: 1 + "em", marginBottom: 1 + "em", width: 50 + "%"}}>
             <h1>{props.name}</h1>
             <h4>Nationality: {props.nationality}</h4>
             <a href={props.url} rel="noopener noreferrer" target="_blank">{props.url}</a>
-            <hr />
         </article>
     );
 }
